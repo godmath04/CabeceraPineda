cfa16809d6369ac38f0d0494f7cbce4991b462f0
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-08 16:13:00 -0500

    Archivos Cabecera

--- source/main.c
+++ source/main.c
@@ -2,7 +2,7 @@
 #include "..\librerias\rresta.h"
 #include "..\librerias\mmultiplicacion.h"
 #include "..\librerias\ddivision.h"
-
+#include "..\librerias\ssuma.h"
 int main()
 {
     int num1, num2;
@@ -10,10 +10,14 @@ int main()
     scanf("%d", &num1);
     printf("Ingrese el 2do numero: ");
     scanf("%d", &num2);
+    printf("La suma es: %d", suma(num1,num2));
+    printf("\n");
     printf("La resta es: %d", resta(num1,num2));
+    printf("\n");
     printf("La division es: %d", division(num1, num2));
-    printf("La multiplicaicon es: %d". multiplicacion(num1, num2));
+    printf("\n");
+    printf("La multiplicaicon es: %d", multiplicacion(num1, num2));
 
-    
+
     return 0;
 }
---
+++ source/suma.c
@@ -0,0 +1,6 @@
+#include "..\librerias\ssuma.h"
+
+int suma(int a, int b)
+{
+    return a + b;
+}
